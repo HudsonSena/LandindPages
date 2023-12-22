725711e88bdf83609b3eccc4fd227744f0e7ff2b
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2023-12-21 21:33:15 -0300

    udpate tailwind css

--- landing-page-hs/src/app/page.tsx
+++ landing-page-hs/src/app/page.tsx
@@ -2,22 +2,22 @@ import { ModeToggle } from './_components/mode-toggle';
 
 export default function Home() {
   return (
-    <main className='p-10'>
-      <section id="intro">        
-        <div className='container flex max-w-[64rem] flex-col items-center text-center gap-4'>
+    <div className='p-4 flex flex-col text-center'>
+      <header id="intro" className='space-y-6 py-8 md:py-12 lg:pt-32'>        
+        <div className='container flex max-w-[64rem] flex-col items-center gap-4'>
           <img className='w-[200px] rounded-[50%]' src="https://avatars.githubusercontent.com/u/107481318?v=4" alt="" />
-          <h1 className='flex m-2 text-primary items-center'>Oi, sou Hudson Diego.<ModeToggle /></h1>
+          <span className='rounded-2xl bg-muted px-4 py-1.5 text-sm font medium flex text-primary items-center'>Oi, sou Hudson Diego.<ModeToggle /></span>
           <p>Acesse um desses links e aproveite a experiência...</p>          
         </div>
-      </section>
-      <section id="links">
-        <h1><a href="">Site Pessoal</a></h1>
-        <h1><a href="">LinkedIn</a></h1>
-        <h1><a href="">GitHub</a></h1>
-        <h1><a href="">Portifólio Dev</a></h1>
-        <h1><a href="">UI/UX Design</a></h1>
-      </section>
-      <section id="create"><p>Landing Page criada por <span>Hudson Sena</span>, com NextJs e TailwindCss</p></section>
-    </main>
+      </header>
+      <main id="links" className='flex flex-col gap-5 text-center w-[20rem] m-auto'>
+        <h1 className='rounded-2xl bg-muted px-4 py-1.5'><a href="">Site Pessoal</a></h1>
+        <h1 className='rounded-2xl bg-muted px-4 py-1.5'><a href="">LinkedIn</a></h1>
+        <h1 className='rounded-2xl bg-muted px-4 py-1.5'><a href="">GitHub</a></h1>
+        <h1 className='rounded-2xl bg-muted px-4 py-1.5'><a href="">Portifólio - Dev</a></h1>
+        <h1 className='rounded-2xl bg-muted px-4 py-1.5'><a href="">Portifólio - UI/UX Design</a></h1>
+      </main>
+      <footer id="create" className='space-y-6 py-8 md:py-12 lg:pb-32 '><p>Landing Page criada por <span>Hudson Sena</span>, com NextJs e TailwindCss</p></footer>
+    </div>
   )
 }
