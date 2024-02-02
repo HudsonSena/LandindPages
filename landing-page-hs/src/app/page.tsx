716fe374cf7b890d6038de36205a0ab45da4e850
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2024-02-01 23:34:43 -0300

    update in home

--- landing-page-hs/src/app/page.tsx
+++ landing-page-hs/src/app/page.tsx
@@ -1,5 +1,6 @@
 import { ModeToggle } from './_components/mode-toggle';
-import { Github, Linkedin, MessageCircle, Code, LayoutTemplate } from  "lucide-react";
+import { FaGithub, FaLinkedin, FaCode } from 'react-icons/fa';
+import { LuLayoutDashboard, LuLayout } from 'react-icons/lu'
 
 export default function Home() {
   return (
@@ -7,22 +8,19 @@ export default function Home() {
       <header id="intro" className='space-y-6 py-8 md:py-12 lg:pt-32'>        
         <div className='container flex max-w-[64rem] flex-col items-center gap-4'>
           <img className='w-[200px] rounded-[50%]' src="https://avatars.githubusercontent.com/u/107481318?v=4" alt="Imagem Diego" />
-          <span className='rounded-2xl bg-muted px-4 py-1.5 text-sm font medium flex text-primary items-center'>Oi, sou Hudson Diego<ModeToggle /></span>
+          <h1 className='rounded-2xl bg-muted px-4 py-1.5 text-sm font medium flex items-center gap-1'>Oi, sou<span className='text-primary flex items-center justify-center'>Hudson Diego<ModeToggle /></span></h1>
           <p>Desenvolvedor FrontEnd e UI/UX Design.<br /> 
           Acesse um desses links e aproveite a experiência!</p>          
         </div>
       </header>
       <main id="links" className='flex flex-col gap-5 text-center w-[20rem] m-auto'>
-        <h1 className='flex flex-col'>
-          <a className='cursor-pointer'>Github<Github /></a>
-          <a>LinkedIn<Linkedin /></a>
-          <a>Whatsapp<MessageCircle /></a>
-          <a>Portifólio Dev<Code /></a>
-          <a>Portifólio UI/UX<LayoutTemplate /></a>
-        </h1>
-        <h1>
-          <button>Professional Website</button>
-        </h1>
+        <section className='flex flex-col gap-4'>
+          <a className='rounded-2xl bg-muted py-2 text-sm font medium flex justify-center items-center gap-2'><FaGithub size={20}/>Github</a>
+          <a className='rounded-2xl bg-muted py-2 text-sm font medium flex justify-center items-center gap-2'><FaLinkedin size={20}/> LinkedIn</a>
+          <a className='rounded-2xl bg-muted py-2 text-sm font medium flex justify-center items-center gap-2'><FaCode size={20}/> Portifólio Dev</a>
+          <a className='rounded-2xl bg-muted py-2 text-sm font medium flex justify-center items-center gap-2'><LuLayoutDashboard size={20} /> Portifólio UI/UX</a>
+          <a className='rounded-2xl bg-muted py-2 text-sm font medium flex justify-center items-center gap-2'><LuLayout size={20}/> Professional WebSite</a>
+        </section>
       </main>
       <footer id="create" className='space-y-6 py-8 md:py-12 lg:pb-32 '><p className='text-xs'>Criado por <span className='text-primary'>Hudson Sena</span>, com NextJs 14 e TailwindCss</p></footer>
     </div>
