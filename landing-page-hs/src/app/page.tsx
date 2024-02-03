c25b61eaa64d0e895da5e43872050d0d19c1ac35
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2024-02-03 12:04:16 -0300

    Update page.tsx

--- landing-page-hs/src/app/page.tsx
+++ landing-page-hs/src/app/page.tsx
@@ -5,7 +5,7 @@ import { LuLayoutDashboard, LuLayout } from 'react-icons/lu'
 export default function Home() {
   return (
     <div className='p-4 flex flex-col text-center'>
-      <header id="intro" className='space-y-6 py-8 md:py-12 lg:pt-32'>        
+      <header id="intro" className='space-y-6 py-8 md:py-12 lg:pt-32 w-[30rem] m-auto'>        
         <div className='container flex max-w-[64rem] flex-col items-center gap-4'>
           <img className='w-[200px] rounded-[50%]' src="https://avatars.githubusercontent.com/u/107481318?v=4" alt="Imagem Diego" />
           <h1 className='rounded-2xl bg-muted px-4 py-1.5 text-sm font medium flex items-center gap-1'>Oi, sou<span className='text-primary flex items-center justify-center'>Hudson Diego<ModeToggle /></span></h1>
@@ -13,7 +13,7 @@ export default function Home() {
           Acesse um desses links e aproveite a experiência!</p>          
         </div>
       </header>
-      <main id="links" className='flex flex-col gap-5 text-center w-[20rem] m-auto'>
+      <main id="links" className='flex flex-col gap-5 text-center w-[20rem] m-auto md:w-[30rem] m-auto'>
         <section className='flex flex-col gap-4'>
           <a className='rounded-2xl bg-muted py-2 text-sm font medium flex justify-center items-center gap-2'><FaGithub size={20}/>Github</a>
           <a className='rounded-2xl bg-muted py-2 text-sm font medium flex justify-center items-center gap-2'><FaLinkedin size={20}/> LinkedIn</a>
